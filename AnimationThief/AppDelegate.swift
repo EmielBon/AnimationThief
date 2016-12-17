11d2b1d73511a15a9f97bfb6b03dfe26de3e1c9a
Emiel Bon <e.bon@itude.com>
2016-12-17 15:18:14 +0100

    Initial commit

---
+++ AnimationThief/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  AnimationThief
+//
+//  Created by Emiel Bon on 17-12-16.
+//  Copyright © 2016 Bla Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}
+
---
+++ AnimationThief/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationThief
+//
+//  Created by Emiel Bon on 17-12-16.
+//  Copyright © 2016 Bla Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
